4a40a4df1aa4bf7828a7f8a5afc4290a5b49da0a
Allie <zxp800310@gmail.com>
2018-09-03 23:00:56 +0800

    create favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903145651) do
+ActiveRecord::Schema.define(version: 20180903145940) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20180903145651) do
 
   create_table "ages", force: :cascade do |t|
     t.string "old"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
