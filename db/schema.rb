930adc546febf6bea5795670eac06c04a4d8e1da
Allie <zxp800310@gmail.com>
2018-09-03 22:47:49 +0800

    create age model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903144033) do
+ActiveRecord::Schema.define(version: 20180903144613) do
+
+  create_table "ages", force: :cascade do |t|
+    t.string "old"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
