aae589f76336ac6dc865ac76ff8b5b2b6013e5f9
Student X <sweewen45@gmail.com>
2018-09-04 23:25:00 +0800

    add column name to ages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904154353) do
+ActiveRecord::Schema.define(version: 20180904154354) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180904154353) do
     t.float "old"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "favorites", force: :cascade do |t|
