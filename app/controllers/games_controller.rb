06f63f86daf90a900c5ca05db05bef4aa41c0f57
Student X <sweewen45@gmail.com>
2018-09-08 04:18:03 +0800

    not done yet, but need to checkout and solve migration problem

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,30 @@
 class GamesController < ApplicationController
   def home
+    
   end
+
+  def random
+    if params[:age_game][:age_id]
+      @age_games = AgeGame.where(age_id: params[:age_game][:age_id]).all
+      age_game_ids = []
+      @age_games.each do |age_game|
+        age_game_ids << age_game.game_id
+      end
+
+      if params[:situation_game][:situation_id]
+        @game = games.find_by(id: params[:situation_game][:situation_id]).all.sample
+
+      else
+        @game = Game.find_by(id: @age_games.sample.game_id)
+      end
+
+    elsif
+      params[:situation_game][:situation_id]
+      @situation_games = SituationGame.where(id: params[:situation_game][:situation_id]).all
+      @game = Game.find_by(id: @situation_games.sample.game_id)
+    else  
+      @game = Game.all.sample
+    end
+  end
+
 end
