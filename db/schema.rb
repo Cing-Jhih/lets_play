e616512529c07dad704f63d1fec57fb9521ddef3
shushu <shushu0212@gami.com>
2018-09-07 08:45:02 +0800

    add column fb_uid and fb_token to users for facebook omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904154354) do
+ActiveRecord::Schema.define(version: 20180907004419) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20180904154354) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fb_uid"
+    t.string "fb_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
