e1c8f50a93243006e837b307754c53747ff9dd8d
shushu <shushu0212@gami.com>
2018-09-09 00:03:45 +0800

    Add feature: sign in with facebook and use figaro to set heroku ENV

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,40 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
   has_many :games
   has_many :replies
   has_many :favorites, dependent: :destroy
   has_many :replied_games, through: :replies, source: :game
   has_many :favorited_games, through: :favorites, source: :game
 
+  def self.from_omniauth(auth)
+    # Case 1: Find existing user by facebook uid
+    user = User.find_by_fb_uid( auth.uid )
+    if user
+      user.fb_token = auth.credentials.token
+      user.save!
+      return user
+    end
+
+    # Case 2: Find existing user by email
+    existing_user = User.find_by_email( auth.info.email )
+    if existing_user
+      existing_user.fb_uid = auth.uid
+      existing_user.fb_token = auth.credentials.token
+      existing_user.save!
+      return existing_user
+    end
+
+    # Case 3: Create new password
+    user = User.new
+    user.fb_uid = auth.uid
+    user.fb_token = auth.credentials.token
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.save!
+    return user
+  end
 end
