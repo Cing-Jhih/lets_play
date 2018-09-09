d45564d832c40e3c06405beebfba333e6f27dd81
Student X <sweewen45@gmail.com>
2018-09-09 18:40:00 +0800

    back to game#home if no game fit selected age and situation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  def home  
+  def home
   end
 
   def show
@@ -7,12 +7,12 @@ class GamesController < ApplicationController
   end
 
   def random
-    age_game_ids = []
+    age_game_ids = [] # 存放所有符合選定年齡的game.id
     AgeGame.where(age_id: params[:age_game][:age_id]).find_each do |age_game|
       age_game_ids << age_game.game_id
     end
 
-    situation_game_ids = []
+    situation_game_ids = [] # 存放所有符合選定情境的game.id
     SituationGame.where("situation_id = ?", params[:situation_game][:situation_id]).find_each do |situation_game|
       situation_game_ids << situation_game.game_id
     end
@@ -28,6 +28,11 @@ class GamesController < ApplicationController
     
     else
       @game = Game.where(id: situation_game_ids & age_game_ids).all.sample
+    end
+
+    if @game == nil
+      flash[:notice] = "糟糕！您指定的玩家年齡與情境，我們找不到遊戲推薦給您Q_Q 請重新設置或進來逛逛其他遊戲"
+      redirect_to root_path
     end
   end
 end
