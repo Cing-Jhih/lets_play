d30f821439552d218785b40b571c8169116b9e07
shushu <shushu0212@gami.com>
2018-09-11 11:58:22 +0800

    devise after login jump original url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_location
   protect_from_forgery with: :exception
+
+  private
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.env['HTTP_REFERER'] unless request.env['HTTP_REFERER'] =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    request.referrer
+  end
  
   protected
 
