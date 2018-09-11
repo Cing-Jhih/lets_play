dfe5cc6af59007cf21aad03cf1515870cc3846f0
Allie <zxp800310@gmail.com>
2018-09-12 06:04:25 +0800

    add favorites count to games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904154354) do
+ActiveRecord::Schema.define(version: 20180911215853) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180904154354) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "favorites_count"
   end
 
   create_table "replies", force: :cascade do |t|
