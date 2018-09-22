ca07201ede17d871e3aa2f69a2ee47533d08fd3a
Student X <sweewen45@gmail.com>
2018-09-22 11:07:44 +0800

    partial:game, ages table and routes for different age#show, modify situation#show action for use game_list partial

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -1,6 +1,11 @@
 class SituationsController < ApplicationController
   def show
     @situation = Situation.find(params[:id])
-    @situation_games = @situation.situation_games.all.order(game_id: :asc)
+    @situation_games = @situation.situation_games.all
+    games_ids = []  
+    @situation_games.find_each do |situation_game|
+      games_ids << situation_game.game
+    end  
+    @games = Game.where(id: games_ids).all.order(created_at: :desc)
   end
 end
---
+++ spec/controllers/ages_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AgesController, type: :controller do
+
+end
