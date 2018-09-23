9da01faf70beec4ce1b39c610f549335b3f02ac6
Allie <zxp800310@gmail.com>
2018-09-23 16:15:05 +0800

    add unfollow button on user index page and destroy action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,7 @@
 class FollowshipsController < ApplicationController
+
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
-
     if @followship.save
       flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
@@ -10,4 +10,12 @@ class FollowshipsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+
+  def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:alert] = "Followship destroyed"
+    redirect_back(fallback_location: root_path)
+  end
+  
 end
