da8414859b015ef0b320317a0b53ccea86bcb9b7
Allie <zxp800310@gmail.com>
2018-09-23 11:11:29 +0800

    create followship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920025116) do
+ActiveRecord::Schema.define(version: 20180923030816) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20180920025116) do
   create_table "favorites", force: :cascade do |t|
     t.integer "game_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "followships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "following_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
