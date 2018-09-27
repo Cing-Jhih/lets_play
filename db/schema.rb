1a48fe1a20bb519e020c9f701ae0c96814a788d9
shushu <shushu0212@gmail.com>
2018-09-27 23:16:11 +0800

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926215638) do
+ActiveRecord::Schema.define(version: 20180927145201) do
 
   create_table "age_games", force: :cascade do |t|
     t.integer "game_id"
@@ -60,6 +60,14 @@ ActiveRecord::Schema.define(version: 20180926215638) do
     t.integer "tag_id"
     t.index ["game_id"], name: "index_games_tags_on_game_id"
     t.index ["tag_id"], name: "index_games_tags_on_tag_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "replies", force: :cascade do |t|
