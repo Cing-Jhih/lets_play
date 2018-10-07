d57d932b1f0044eb184061b6549a675bb9df849d
shushu <shushu0212@gmail.com>
2018-10-07 16:37:23 +0800

    use ajax to handle followships links

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -5,10 +5,8 @@ class FollowshipsController < ApplicationController
     @followship = current_user.followships.build(following_id: params[:following_id])
     if @followship.save
       flash[:notice] = "Successfully followed"
-      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -16,7 +14,6 @@ class FollowshipsController < ApplicationController
     @followship = current_user.followships.where(following_id: params[:id]).first
     @followship.destroy
     flash[:alert] = "Followship destroyed"
-    redirect_back(fallback_location: root_path)
   end
   
 end
